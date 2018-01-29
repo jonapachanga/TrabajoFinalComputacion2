9dbbc7d5621cd6410a43541040438724c614348b
jonapachanga <jonatan.eseiza@gmail.com>
2018-01-29 18:52:48 -0300

    se crea main en server

---
+++ server/main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<unistd.h>
+#include<string.h>
+#include<fcntl.h>
+#include<errno.h>
+#include<netinet/in.h>
+#include<sys/socket.h>
+#include <arpa/inet.h>
+
+int main(int argc, char** argv) 
+{
+    
+}
